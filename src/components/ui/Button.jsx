3f23203fdfe597d8ab0fab9c31318080cb4c3dec
Mabaidul-work <mabaidul@appscrip.co>
2024-10-25 17:12:36 +0530

    add: reusable button added

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,18 +1,16 @@
 "use client";
 import { cn } from "@/utils";
 import { cva } from "class-variance-authority";
-import React, { forwardRef } from "react";
+import { forwardRef } from "react";
 
 const buttonStyles = cva(
-  [
-    "w-32 lg:w-48 h-9 md:h-12 lg:h-14 text-sm md:text-base lg:text-lg font-bold",
-  ],
+  ["w-32 lg:w-48 h-12 lg:h-14 text-sm md:text-base lg:text-lg font-bold"],
   {
     variants: {
       variant: {
         primary:
           "text-white bg-gradient-to-r from-primary-500 to-secondary-500 rounded-full",
-        secondary: "border border-gray-100 rounded-full gradient-text",
+        secondary: "border border-primary-500 rounded-full gradient-text",
       },
     },
     defaultVariants: {
@@ -20,11 +18,12 @@ const buttonStyles = cva(
     },
   }
 );
-const Button = forwardRef(({ variant, className, ...props }, ref) => {
+const Button = forwardRef(({ variant, className, onClick, ...props }, ref) => {
   return (
     <button
       ref={ref}
       className={cn(buttonStyles({ variant }), className)}
+      onClick={onClick}
       {...props}
     />
   );
