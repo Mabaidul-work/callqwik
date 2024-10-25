6564ac74605597025db5beebb75551da8e459c20
Mabaidul-work <mabaidul@appscrip.co>
2024-10-25 17:07:04 +0530

    fix: header sticky

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,10 @@
 import { WEBSITE_LOGO } from "@/constants/images";
 import Image from "next/image";
-import React from "react";
 import { Button } from "../ui";
 
 const Header = () => {
   return (
-    <header className="w-full h-20 lg:h-24">
+    <header className="w-full h-20 lg:h-24 sticky z-10 top-0 bg-white">
       <div className="max-w-screen-1.5xl mx-auto h-full flex items-center justify-between px-4 md:px-8 lg:px-16 py-3 lg:py-5">
         <div className="">
           <Image
