5cfacf0ee964854cbc621e9d1e969f3e242a0280
Mabaidul-work <mabaidul@appscrip.co>
2024-10-25 17:05:59 +0530

    all the dynamic data are created in the data.js file

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,3 +1,48 @@
+import {
+  ACTIVEPIECES_LOGO,
+  AGENCIES,
+  AI_INTENT_MEASUREMENT,
+  AI_POST_CALL_ACTION,
+  APPSCRIP_LOGO,
+  CALL_ICON,
+  CRM_INTEGRATION,
+  CUSTOM_ACTIONS,
+  EAZY_LOGO,
+  EDUCATION,
+  GETFUDO_LOGO,
+  GLOBE_ICON,
+  HEALTHCARE,
+  HOME_SERVICES,
+  HOSPITALITY,
+  HUB_SPOT_LOGO,
+  IM_INTEGRATION,
+  INSTAGRAM_ICON,
+  INSTANT_CONNECTION,
+  LABORMATE_LOGO,
+  LANGUAGE_ICON,
+  METTA_MOUSE_LOGO,
+  MOBILE_FIRST_APPROACH,
+  ONE_ON_ONE_CONVERSATION,
+  PERSON_GROUP_ICON,
+  PHONE_NUMBER_LINK,
+  PREFERRED_CRM,
+  RB2B_LOGO,
+  REAL_ESTATE,
+  SALES_MEETINGS,
+  SALESFORCE_LOGO,
+  TESTIMONIAL_ONE,
+  TESTIMONIAL_THREE,
+  TESTIMONIAL_TWO,
+  TIKTOK_ICON,
+  TRACKER_ICON,
+  TRUELY_FREE_MARKET_LOGO,
+  TWITTER_ICON,
+  VOICE_ASSISTANT,
+  WEBSITE_TRAFFIC,
+  YOUTUBE_ICON,
+  ZAPIER_LOGO,
+} from "./images";
+
 export const salesMagnetListItems = [
   {
     id: 1,
@@ -12,3 +57,424 @@ export const salesMagnetListItems = [
     text: "Video or Audio Call Customers Directly from Website",
   },
 ];
+
+export const trustedCompanies = [
+  {
+    id: 1,
+    title: "LaborMate",
+    icon: LABORMATE_LOGO,
+  },
+  {
+    id: 2,
+    title: "GetFudo",
+    icon: GETFUDO_LOGO,
+  },
+  {
+    id: 3,
+    title: "TrulyFree Market",
+    icon: TRUELY_FREE_MARKET_LOGO,
+  },
+  {
+    id: 4,
+    title: "Appscrip",
+    icon: APPSCRIP_LOGO,
+  },
+  {
+    id: 5,
+    title: "MettaMouse",
+    icon: METTA_MOUSE_LOGO,
+  },
+  {
+    id: 6,
+    title: "Eazy",
+    icon: EAZY_LOGO,
+  },
+];
+
+export const salesChallanges = [
+  {
+    id: 1,
+    title: "Missed Calls",
+    icon: CALL_ICON,
+    description:
+      "Small teams risk losing valuable leads with unanswered calls, resulting in missed revenue opportunities.",
+  },
+  {
+    id: 2,
+    title: "Expensive Intent Trackers",
+    icon: TRACKER_ICON,
+    description:
+      "Without real-time alerts, businesses fail to act on high-intent website visitors, missing crucial engagement chances.",
+  },
+  {
+    id: 3,
+    title: "Costly Staffing",
+    icon: PERSON_GROUP_ICON,
+    description:
+      "Hiring 24/7 staff for lead management is expensive and unsustainable for many businesses.",
+  },
+  {
+    id: 4,
+    title: "Language Barriers",
+    icon: LANGUAGE_ICON,
+    description:
+      "Without effective multilingual support, businesses struggle to engage international customers, limiting global growth potential.",
+  },
+  {
+    id: 5,
+    title: "No mobile accesssibility",
+    icon: GLOBE_ICON,
+    description:
+      "Sales teams miss crucial calls without a mobile-first solution, limiting their ability to answer from anywhere, anytime.",
+  },
+];
+
+export const chatAndCallFeatures = [
+  {
+    id: 1,
+    title: "24/7 Voice Assistant to Handle Every Call",
+    description:
+      "AI voice assistants handle all your inbound calls 24/7, ensuring you never miss important leads or inquiries",
+    imageUrl: VOICE_ASSISTANT,
+  },
+  {
+    id: 2,
+    title: "Tailored 1-on-1 Conversations for Personalized Sales",
+    description:
+      "Deliver personalized, tailored conversations to each visitor, creating a more engaging, effective, and sales-driven experience",
+    imageUrl: ONE_ON_ONE_CONVERSATION,
+  },
+  {
+    id: 3,
+    title: "Instantly Connect with ICPs When They're Online",
+    description:
+      "Engage with your ideal customers instantly when they are online, increasing engagement and maximizing conversion opportunities",
+    imageUrl: INSTANT_CONNECTION,
+  },
+  {
+    id: 4,
+    title: "Seamless AI Handover to Human Agents with Integrated IM",
+    description:
+      "Effortlessly transfer high-intent customers from AI to human agents, using real-time integrated instant messaging for better service",
+    imageUrl: IM_INTEGRATION,
+  },
+  {
+    id: 5,
+    title: "AI Pushes Leads to Your Preferred CRM",
+    description:
+      "Automatically capture lead data and push it to your CRM in real-time, keeping your sales pipeline updated",
+    imageUrl: PREFERRED_CRM,
+  },
+  {
+    id: 6,
+    title: "Convert Prospects into Sales Meetings Effortlessly",
+    description:
+      "Convert website visitors into sales meetings by summarizing conversations into MoMs and automatically scheduling appointments",
+    imageUrl: SALES_MEETINGS,
+  },
+  {
+    id: 7,
+    title: "Link your phone number for free",
+    description:
+      "Link your number to the AI agent for free, allowing it to transfer calls or answer on your behalf",
+    imageUrl: PHONE_NUMBER_LINK,
+  },
+];
+
+export const salesAndCRMFeatures = [
+  {
+    id: 1,
+    title: "Free CRM Integration",
+    description:
+      "Free integration with any CRM’s - support and sales, schedule and order management.",
+    imageUrl: CRM_INTEGRATION,
+  },
+  {
+    id: 2,
+    title: "Custom Actions",
+    description:
+      "Create customized actions and seamlessly integrate with any CRM or tool to enhance the functionality of your AI assistant",
+    imageUrl: CUSTOM_ACTIONS,
+  },
+  {
+    id: 3,
+    title: "Mobile-First Approach",
+    description:
+      "Our mobile-first solution allows sales agents to take calls from anywhere, eliminating the need for a desktop and ensuring no leads are missed.",
+    imageUrl: MOBILE_FIRST_APPROACH,
+  },
+  {
+    id: 4,
+    title: "AI Intent Measurement",
+    description:
+      "Analyzes visitors to classify them as high or low intent using tools like RB2B  & 6sense, alerts sales agent of high-intent users.",
+    imageUrl: AI_INTENT_MEASUREMENT,
+  },
+  {
+    id: 5,
+    title: "AI Post Call Actions",
+    description:
+      "The AI receptionist summarises conversations and generates meeting minutes for the sales team after each call.",
+    imageUrl: AI_POST_CALL_ACTION,
+  },
+  {
+    id: 6,
+    title: "Convert Website Traffic",
+    description:
+      "Connecting visitors to sales agent whenever they are ready for a human conversation",
+    imageUrl: WEBSITE_TRAFFIC,
+  },
+];
+
+export const usecases = [
+  {
+    id: 1,
+    title: "Healthcare",
+    description:
+      "AI receptionists provide 24/7 support for appointment scheduling, patient inquiries, and emergency calls. They log symptoms, suggest minor help, schedule doctor visits, and send confirmation push notifications, ensuring seamless care management.",
+    imageUrl: HEALTHCARE,
+  },
+  {
+    id: 2,
+    title: "Real Estate",
+    description:
+      "AI receptionists handle inquiries, schedule property viewings, and manage client follow-ups, streamlining agents’ workflows and enhancing client engagement by enabling sales teams to focus on closing deals more effectively.",
+    imageUrl: REAL_ESTATE,
+  },
+  {
+    id: 3,
+    title: "Agencies",
+    description:
+      "AI receptionists assist agencies by managing client inquiries, scheduling consultations, sending call details to the respective teams, and recording them in the database. They also write minutes of meetings (MOM) to ensure teams fully understand client requirements.",
+    imageUrl: AGENCIES,
+  },
+  {
+    id: 4,
+    title: "Home Services",
+    description:
+      "AI receptionists handle large volumes of appointment bookings and urgent service calls for cleaning, HVAC, plumbing, and repairs. They record service details, schedule appointments, and send confirmations through SMS.",
+    imageUrl: HOME_SERVICES,
+  },
+  {
+    id: 5,
+    title: "Hospitality",
+    description:
+      "AI receptionists automate booking management, guest inquiries, and provide 24/7 customer support. They streamline the booking process and send confirmations, enhancing guest experience and operational efficiency.",
+    imageUrl: HOSPITALITY,
+  },
+  {
+    id: 6,
+    title: "Education",
+    description:
+      "AI automates enrollment inquiries, schedules meetings, and provides after-hours support for schools, universities, and training institutes, ensuring smooth communication and management of student interactions.",
+    imageUrl: EDUCATION,
+  },
+];
+
+export const integrationCompanies = [
+  {
+    id: 1,
+    title: "rb2b",
+    icon: RB2B_LOGO,
+  },
+  {
+    id: 2,
+    title: "activepices",
+    icon: ACTIVEPIECES_LOGO,
+  },
+  {
+    id: 3,
+    title: "zapier",
+    icon: ZAPIER_LOGO,
+  },
+  {
+    id: 4,
+    title: "hubspot",
+    icon: HUB_SPOT_LOGO,
+  },
+  {
+    id: 5,
+    title: "salesforce",
+    icon: SALESFORCE_LOGO,
+  },
+];
+
+export const pricingPlans = [
+  {
+    _id: 1,
+    planName: "30 Days Trial Access Plan",
+    price: "Free",
+    benifits: [
+      { id: 11, text: "2000 Credits" },
+      { id: 12, text: "Free Unlimited Messaging" },
+    ],
+    buttonLabel: "Select",
+    isPlanActive: false,
+  },
+  {
+    _id: 2,
+    planName: "Basic Access Plan",
+    price: "$25.00/mo",
+    benifits: [
+      { id: 21, text: "2000 Credits" },
+      { id: 22, text: "Free Unlimited Messaging" },
+    ],
+    buttonLabel: "Select",
+    isPlanActive: true,
+  },
+];
+
+export const featureDetails = [
+  {
+    section: "AI Agent Features",
+    features: [
+      { id: 1, text: "Support For Multiple TTS Providers" },
+      { id: 2, text: "Receive audio calls directly on your phone" },
+      { id: 3, text: "Receive video calls directly on your phone" },
+      { id: 4, text: "Call website visitors directly from the app" },
+      { id: 5, text: "Get alerted when a contact is created on your site" },
+      { id: 6, text: "Get alerted when an ICP is on your site" },
+      { id: 7, text: "View contact information" },
+      { id: 8, text: "View lead information" },
+    ],
+  },
+  {
+    section: "AI Voice Agent",
+    features: [
+      {
+        id: 1,
+        text: "Support For Multiple TTS Providers - Azure, Gemini, OpenAI",
+      },
+      { id: 2, text: "Support For Multiple STT Providers - OpenAI, Deepgram" },
+      { id: 3, text: "Function Calls" },
+      {
+        id: 4,
+        text: "Knowledge Base support for PDFs, JSON, Website, Multiple Page URLs",
+      },
+      { id: 5, text: "Multiple voice selection" },
+      { id: 6, text: "Support for multiple languages" },
+      { id: 7, text: "Welcome message" },
+      {
+        id: 8,
+        text: "Connect with your phone number via Twilio, Plivo, Telnyx",
+      },
+      { id: 9, text: "Control voice & response sentiment/sensitivities" },
+      { id: 10, text: "Auto call trigger" },
+      { id: 11, text: "Have voice agent query user when user goes silent" },
+      { id: 12, text: "Manage background sensitiveness" },
+    ],
+  },
+  {
+    section: "AI CRM",
+    features: [
+      { id: 1, text: "AI creates contacts" },
+      { id: 2, text: "AI generates call summary" },
+      { id: 3, text: "AI measures sale performance over call" },
+      { id: 4, text: "AI measures agent purchase intent" },
+      { id: 5, text: "AI creates lead and connects to connected CRM" },
+      { id: 6, text: "Website visitor tracking" },
+      { id: 7, text: "Personalized messaging for each website visitor" },
+    ],
+  },
+  {
+    section: "Agent App",
+    features: [
+      { id: 1, text: "Track website visitors in real time" },
+      { id: 2, text: "Receive audio calls directly on your phone" },
+      { id: 3, text: "Receive video calls directly on your phone" },
+      { id: 4, text: "Call website visitors directly from the app" },
+      { id: 5, text: "Get alerted when a contact is created on your site" },
+      { id: 6, text: "Get alerted when an ICP is on your site" },
+      { id: 7, text: "View contact information" },
+      { id: 8, text: "View lead information" },
+    ],
+  },
+];
+
+export const testimonials = [
+  {
+    id: 1,
+    name: "Sarah Mitchell",
+    role: "CEO of Tech Solutions",
+    image: TESTIMONIAL_ONE,
+    testimonial:
+      "Amazing chatbot! It has revolutionized the way we handle customer queries. The personalized conversations and quick handover to our sales team have increased our conversions significantly.",
+  },
+  {
+    id: 2,
+    name: "Emily Rodriguez",
+    role: "Head of Sales at GrowthLab",
+    image: TESTIMONIAL_TWO,
+    testimonial:
+      "We've integrated the chatbot into our CRM, and it has completely streamlined our sales process. The AI-generated leads are incredibly accurate and have boosted our outreach.",
+  },
+  {
+    id: 3,
+    name: "John Taylor",
+    role: "Founder of LeadGenX",
+    image: TESTIMONIAL_THREE,
+    testimonial:
+      "Our customer satisfaction levels have gone through the roof thanks to the personalized assistance the chatbot provides. It's like having a sales agent on-call 24/7.",
+  },
+  {
+    id: 4,
+    name: "Emily Rodriguez duplicate",
+    role: "Head of Sales at GrowthLab",
+    image: TESTIMONIAL_TWO,
+    testimonial:
+      "We've integrated the chatbot into our CRM, and it has completely streamlined our sales process. The AI-generated leads are incredibly accurate and have boosted our outreach.",
+  },
+  {
+    id: 5,
+    name: "John Taylor dulicate",
+    role: "Founder of LeadGenX",
+    image: TESTIMONIAL_THREE,
+    testimonial:
+      "Our customer satisfaction levels have gone through the roof thanks to the personalized assistance the chatbot provides. It's like having a sales agent on-call 24/7.",
+  },
+];
+
+export const socialMediaIcons = [
+  {
+    icon: INSTAGRAM_ICON,
+    title: "instagram-icon",
+    altText: "Instagram",
+    link: "#",
+  },
+  {
+    icon: TWITTER_ICON,
+    title: "twitter-icon",
+    altText: "Twitter",
+    link: "#",
+  },
+  {
+    icon: TIKTOK_ICON,
+    title: "tiktok-icon",
+    altText: "TikTok",
+    link: "#",
+  },
+  {
+    icon: YOUTUBE_ICON,
+    title: "youtube-icon",
+    altText: "YouTube",
+    link: "#",
+  },
+];
+
+export const footerNavLinks = {
+  COMPANY: [
+    { name: "About", href: "#" },
+    { name: "Contact", href: "#" },
+  ],
+  COMPARE: [
+    { name: "vs Drift", href: "#t" },
+    { name: "vs Clearbit", href: "#" },
+    { name: "vs Qualified", href: "#" },
+    { name: "vs Leadfeeder", href: "#" },
+    { name: "vs Zoominfo", href: "#" },
+  ],
+  LEGAL: [
+    { name: "Terms of Service", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+  ],
+};
