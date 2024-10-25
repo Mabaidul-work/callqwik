c415edcb4cd4e784d35910c3019cd36a8230c791
Mabaidul-work <mabaidul@appscrip.co>
2024-10-25 17:08:35 +0530

    fix: hero thumbnail added, and fix input and button height

--- src/components/hero/HeroSection.jsx
+++ src/components/hero/HeroSection.jsx
@@ -1,18 +1,22 @@
 "use client";
-import React from "react";
-import { Button, UnorderedList } from "../ui";
 import { salesMagnetListItems } from "@/constants/data";
+import { THUMNAIL_HERO } from "@/constants/images";
+import Image from "next/image";
+import { Button, UnorderedList } from "../ui";
 
 const HeroSection = () => {
   return (
     <section className="w-full">
-      <div className="max-w-screen-1.5xl mx-auto w-full px-4 md:px-16 lg:px-28 py-10 md:py-14 lg:py-16 flex flex-col lg:flex-row items-start lg:items-center justify-center gap-6 md:gap-10 lg:gap-14">
+      <div className="max-w-screen-1.5xl mx-auto w-full px-4 md:px-16 lg:px-16 xl:px-28 py-10 md:py-14 lg:py-16 flex flex-col lg:flex-row items-start lg:items-center justify-center gap-6 md:gap-10 xl:gap-14">
         {/* LEFT SIDE */}
-        <div className="w-full lg:w-1/2 flex flex-col gap-5 lg:gap-9 items-center">
-          <h1 className="font-extrabold text-2.5xl lg:text-4.5xl">
+        <div className="w-full lg:w-1/2 flex flex-col gap-5 lg:gap-6 xl:gap-9 items-center">
+          <h1 className="font-extrabold text-2.5xl lg:3xl xl:text-4.5xl">
             Transform your website into a sales magnet using{" "}
             <span className="lg:hidden gradient-text">
               No missed calls, Just results.
+            </span>
+            <span className="hidden lg:block gradient-text mt-1">
+              AI Call & Chat Assistants.
             </span>
           </h1>
 
@@ -30,31 +34,44 @@ const HeroSection = () => {
                 placeholder="Enter Email Address"
                 className="bg-black-50 lg:bg-transparent  rounded-full lg:border lg:border-primary-500 w-full h-12 text-sm md:text-base lg:text-lg font-medium text-black-300 px-6 lg:max-w-80 focus:outline-green-500"
               />
-              <Button variant="primary" className="h-12 w-full">
+              <Button variant="primary" className="h-12 lg:h-12 w-full">
                 Join wishlist
               </Button>
             </div>
           </div>
         </div>
         {/* RIGHT SIDE: VIDEO PART */}
-        <div className="w-full lg:w-1/2 min-h-56 lg:h-96 border-2 border-gray-200 rounded-lg flex items-center justify-center">
-          <video controls className="w-full min-h-56 h-full rounded-lg">
+        <div className="w-full lg:w-1/2 h-full flex items-center justify-center">
+          <Image
+            src={THUMNAIL_HERO}
+            alt="Thumbnail Hero"
+            width={500}
+            height={600}
+            className="w-full h-full"
+            priority
+            sizes="(max-width: 768px) 100vw, 384px"
+            srcSet={`${THUMNAIL_HERO}?w=192 192w,
+                    ${THUMNAIL_HERO}?w=384 384w,
+                    ${THUMNAIL_HERO}?w=768 768w`}
+          />
+
+          {/* <video controls className="w-full min-h-56 h-full rounded-lg">
             <source src="/videos/sample.mp4" type="video/mp4" />
             Your browser does not support the video tag.
-          </video>
+          </video> */}
         </div>
 
         <div className="flex lg:hidden flex-col items-center lg:items-start justify-center gap-3 lg:gap-4 w-full px-4 lg:px-0">
           <p className="text-black-500 font-bold text-base md:text-lg lg:text-2xl">
             6 Months free for first 100 users
           </p>
-          <div className="flex flex-col lg:flex-row items-center gap-3 w-full">
+          <div className="flex flex-col lg:flex-row items-center gap-3 w-full h-full">
             <input
               type="email"
               name="email"
               id="email"
               placeholder="Enter Email Address"
-              className="bg-black-50 lg:bg-transparent  rounded-full lg:border lg:border-primary-500 w-full h-12 text-sm md:text-base lg:text-lg font-medium text-black-300 px-6 lg:max-w-80 focus:outline-green-500"
+              className="bg-black-50 lg:bg-transparent  rounded-full lg:border lg:border-primary-500 w-full h-12 text-sm md:text-base lg:text-lg font-medium text-black-300 px-6  lg:max-w-80 focus:outline-green-500"
             />
             <Button variant="primary" className="h-12 w-full">
               Join wishlist
