3ce0c0b258fb3c43452f91f4062afd014b99356b
Mabaidul-work <mabaidul@appscrip.co>
2024-10-25 16:42:36 +0530

    image variable created for all the images in image.js file

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,2 +1,67 @@
 export const WEBSITE_LOGO = "/images/website-logo.svg";
+export const WEBSITE_LOGO_WHITE = "/images/website-logo-white.svg";
+
 export const TICK_ICON = "/images/icons/tick-icon.svg";
+export const APPSCRIP_LOGO = "/images/icons/company-appscrip-logo.svg";
+export const EAZY_LOGO = "/images/icons/company-eazy-logo.svg";
+export const GETFUDO_LOGO = "/images/icons/company-getfudo-logo.svg";
+export const LABORMATE_LOGO = "/images/icons/company-labormate-logo.svg";
+export const METTA_MOUSE_LOGO = "/images/icons/company-metta-mouse-logo.svg";
+export const TRUELY_FREE_MARKET_LOGO =
+  "/images/icons/company-truelyfree-market-logo.svg";
+
+export const CALL_ICON = "/images/icons/call-icon.svg";
+export const GLOBE_ICON = "/images/icons/globe-icon.svg";
+export const LANGUAGE_ICON = "/images/icons/language-icon.svg";
+export const PERSON_GROUP_ICON = "/images/icons/person-group-icon.svg";
+export const TRACKER_ICON = "/images/icons/tracker-icon.svg";
+
+export const IM_INTEGRATION = "/images/steps/im-integration.png";
+export const INSTANT_CONNECTION = "/images/steps/instant-connection.png";
+export const VOICE_ASSISTANT = "/images/steps/voice-assistant.png";
+export const PREFERRED_CRM = "/images/steps/preferred-crm.png";
+export const ONE_ON_ONE_CONVERSATION =
+  "/images/steps/one-on-one-conversation.png";
+export const SALES_MEETINGS = "/images/steps/sales-meetings.png";
+export const PHONE_NUMBER_LINK = "/images/steps/link-your-phone-number.png";
+
+export const AI_INTENT_MEASUREMENT = "/images/steps/ai-intent-measurement.png";
+export const AI_POST_CALL_ACTION = "/images/steps/ai-post-call-actions.png";
+export const CRM_INTEGRATION = "/images/steps/crm-integraton.png";
+export const CUSTOM_ACTIONS = "/images/steps/custom-actions.png";
+export const MOBILE_FIRST_APPROACH = "/images/steps/mobile-first-approach.png";
+export const WEBSITE_TRAFFIC = "/images/steps/website-traffic.png";
+
+export const HEALTHCARE = "/images/steps/healthcare.png";
+export const REAL_ESTATE = "/images/steps/real-estate.png";
+export const AGENCIES = "/images/steps/agencies.png";
+export const HOME_SERVICES = "/images/steps/home-service.png";
+export const HOSPITALITY = "/images/steps/hospitality.png";
+export const EDUCATION = "/images/steps/education.png";
+
+export const OPEN_IN_NEW = "/images/icons/open-in-new.svg";
+
+export const ACTIVEPIECES_LOGO = "/images/icons/activepieces-logo.svg";
+export const HUB_SPOT_LOGO = "/images/icons/hub-spot-logo.svg";
+export const RB2B_LOGO = "/images/icons/rb2b-logo.svg";
+export const SALESFORCE_LOGO = "/images/icons/salesforce-logo.svg";
+export const ZAPIER_LOGO = "/images/icons/zapier-logo.svg";
+
+export const UP_ARROW = "/images/icons/up-arrow.svg";
+export const DOWN_ARROW = "/images/icons/down-arrow.svg";
+
+export const TESTIMONIAL_ONE = "/images/testimonials-one.svg";
+export const TESTIMONIAL_TWO = "/images/testimonials-two.svg";
+export const TESTIMONIAL_THREE = "/images/testimonials-three.svg";
+
+export const ARROW_LEFT = "/images/icons/chevron-left.svg";
+export const ARROW_RIGHT = "/images/icons/chevron-right.svg";
+
+export const SEAL_CHECK_FILL = "/images/icons/seal-check-fill.svg";
+
+export const INSTAGRAM_ICON = "/images/icons/insta.svg";
+export const TWITTER_ICON = "/images/icons/twitter.svg";
+export const TIKTOK_ICON = "/images/icons/tiktok.svg";
+export const YOUTUBE_ICON = "/images/icons/youtube.svg";
+
+export const THUMNAIL_HERO = "/images/hero-thumbnail.webp";
